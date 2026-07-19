2544a74fd2456017da148d9ef757ccd66388ac6d
Mohannad <alshboul210@gmail.com>
2026-07-19 18:32:39 +0300

    Delete Homework3 directory

--- Homework3/homework3_Part2.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  print("part2: printing the numbers from 1-100 without 41:");
-  for (int number = 1; number <= 100; number++) {
-    if (number == 41) continue;
-    print(number);
-  }
-}
