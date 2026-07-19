9762e6912c75dd4894cb166dbbf198964492a058
Mohannad <alshboul210@gmail.com>
2026-07-19 18:32:20 +0300

    Delete Homework1 directory

--- Homework1/homework1.dart
+++
@@ -1,6 +0,0 @@
-void main() {
-  int distance = 25; //km
-  int speed = 40; //km/h
-  double timeTaken = (distance / speed) * 60;
-  print("the time taken to reach your office: $timeTaken minutes");
-}
